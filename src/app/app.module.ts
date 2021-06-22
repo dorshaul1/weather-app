3baca93c8be017cb9a7804ef15f90fc5e0b93b4b
dorshaul1 <dor.asti@gmail.com>
2021-06-22 13:50:58 +0300

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,12 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 
-import { reducers, metaReducers } from './store/store';
-import { AppEffects } from './store/app.effects';
 
 import { AppComponent } from './root-component/index';
 import { HomeComponent } from './pages/home/home.component';
 import { ItemAppComponent } from './pages/item-app/item-app.component';
 
 import { AppHeaderComponent } from './cmps/app-header/app-header.component';
-import { ItemListComponent } from './cmps/item-list/item-list.component';
-import { ItemPreviewComponent } from './cmps/item-preview/item-preview.component';
-import { ItemEditComponent } from './cmps/item-edit/item-edit.component';
 import { WeatherListComponent } from './cmps/weather-list/weather-list.component';
 
 @NgModule({
@@ -27,28 +22,25 @@ import { WeatherListComponent } from './cmps/weather-list/weather-list.component
     AppComponent,
     HomeComponent,
     ItemAppComponent,
-    ItemEditComponent,
     AppHeaderComponent,
-    ItemListComponent,
-    ItemPreviewComponent,
     WeatherListComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true,
-      },
-    }),
+    // StoreModule.forRoot(reducers, {
+    //   metaReducers,
+    //   runtimeChecks: {
+    //     strictStateImmutability: true,
+    //     strictActionImmutability: true,
+    //   },
+    // }),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production,
     }),
-    EffectsModule.forRoot([AppEffects]),
+    // EffectsModule.forRoot([AppEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
